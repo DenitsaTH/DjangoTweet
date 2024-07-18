201518332d22bc6d4aad82bcefce0a2ba7e29b91
DenitsaTH <denizza.cristova@gmail.com>
2024-07-18 21:54:44 +0300

    tasks: switch gte to lte

--- posts/tasks.py
+++ posts/tasks.py
@@ -9,7 +9,7 @@ def delete_old_posts():
     print(f"Running task at {timezone.now()}")
 
     threshold = timezone.now() - timedelta(seconds=30)
-    deleted_posts = Post.objects.filter(is_deleted=True, deleted_at__gte=threshold)
+    deleted_posts = Post.objects.filter(is_deleted=True, deleted_at__lte=threshold)
 
     print(f"Found {deleted_posts.count()} posts to delete")
 
