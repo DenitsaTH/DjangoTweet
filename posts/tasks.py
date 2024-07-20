d87aff99181d413d3e2c0465a986ee66495be364
DenitsaTH <denizza.cristova@gmail.com>
2024-07-20 23:22:02 +0300

    formatting fix

--- posts/tasks.py
+++ posts/tasks.py
@@ -9,10 +9,11 @@ def delete_old_posts():
     print(f"Running task at {timezone.now()}")
 
     threshold = timezone.now() - timedelta(seconds=30)
-    deleted_posts = Post.objects.filter(is_deleted=True, deleted_at__lte=threshold)
+    deleted_posts = Post.objects.filter(
+        is_deleted=True, deleted_at__lte=threshold)
 
     print(f"Found {deleted_posts.count()} posts to delete")
 
     deleted_posts.delete()
-    
+
     print("Old posts deleted")
