9f49706f0c48a7d65d92e4e9e1bb9ec0eb0116ad
DenitsaTH <denizza.cristova@gmail.com>
2024-09-07 12:48:15 +0300

    fix: incorrect function name in weather url

--- forecast/urls.py
+++ forecast/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from forecast.api import get_forecast
+from forecast.api import get_weather_forecast
 
 
 app_name = 'forecast'
 
 urlpatterns = [
-    path('weather/', get_forecast, name='get_forecast'),
+    path('weather/', get_weather_forecast, name='get_weather_forecast'),
 ]
